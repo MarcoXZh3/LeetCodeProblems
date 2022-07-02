fcc05781b6d45d4aba788da2a1bc7d45c8715c83
MarcoXZh3 <marcoxzh3@gmail.com>
2022-07-02 13:13:48 -0600

    Task 0001 - Two Sum: finished

--- group01/task0001-two-sum/twosum.js
+++ group01/task0001-two-sum/twosum.js
@@ -1,10 +1,66 @@
 /**
+ * Two sum: brutal force of two-level nested iterations
+ * @param {number[]}  nums      the input array of numbers
+ * @param {number}    target    the target sum to search
+ * @return {number[]}           the indexes of the two numbers
+ */
+ const twoSum1 = function(nums, target) {
+    // brutal force to check every combination
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+    return [];
+};
+
+
+/**
+ * Two sum: hash map plus search
+ * trade speed with space
+ * @param {number[]}  nums      the input array of numbers
+ * @param {number}    target    the target sum to search
+ * @return {number[]}           the indexes of the two numbers
+ */
+const twoSum2 = function(nums, target) {
+    const map = {};
+    // first create a hash map
+    for (let i = 0; i < nums.length; i++) {
+        map[`${nums[i]}`] = i;
+    }
+    // then search target using the hash map
+    for (let i = 0; i < nums.length; i++) {
+        const num = target - nums[i];
+        if (map[`${num}`] >= 0 && i !== map[`${num}`]) {
+            return [i, map[`${num}`]];
+        }
+    }
+    return [];
+};
+
+
+/**
+ * Two sum: hash map while search
+ * trade speed with space
  * @param {number[]}  nums      the input array of numbers
  * @param {number}    target    the target sum to search
  * @return {number[]}           the indexes of the two numbers
  */
 const twoSum = function(nums, target) {
-
+    const map = {};
+    // create a hash map
+    for (let i = 0; i < nums.length; i++) {
+        const num = target - nums[i];
+        // search target at the same time: return if found
+        if (map[`${num}`] >= 0) {
+            return [i, map[`${num}`]];
+        }
+        // keep creating the hash map if not found
+        map[`${nums[i]}`] = i;
+    }
+    return [];
 };
 
 
@@ -13,12 +69,19 @@ const twoSum = function(nums, target) {
  * @param {function}  callback    the callback function
  */
 const main = function() {
-  console.log('Task 0001 - Two Sum:');
-  const nums = [2, 7, 11, 15];
-  const target = 9;
-  console.log(`  target=${target}, nums=[${nums.join(", ")}]`);
-  re = twoSum(nums, target) || [];
-  console.log(`  result=[${re.join(", ")}]`);
+    console.log('Task 0001 - Two Sum:');
+
+    var nums = [2, 7, 11, 15];
+    var target = 9;
+    console.log(`  target=${target}, nums=[${nums.join(", ")}]`);
+    re = twoSum(nums, target) || [];
+    console.log(`  result=[${re.join(", ")}]`);
+
+    nums = [3, 2, 4];
+    target = 6;
+    console.log(`  target=${target}, nums=[${nums.join(", ")}]`);
+    re = twoSum(nums, target) || [];
+    console.log(`  result=[${re.join(", ")}]`);
 };
 
 
--- group01/task0001-two-sum/twosum.py
+++ group01/task0001-two-sum/twosum.py
@@ -1,20 +1,70 @@
+def twoSum1(nums: list[int], target: int) -> list[int]:
+    """
+    Two sum: brutal force of two-level nested iterations.
+    Args:
+        nums    the input array of numbers.
+        target  the target sum to search.
+    Returns:
+        the indexes of the two numbers.
+    """
+    for i in range(len(nums) - 1):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return []
+
+
+def twoSum2(nums: list[int], target: int) -> list[int]:
+    """
+    Two sum: hash map plus search.
+    Args:
+        nums    the input array of numbers.
+        target  the target sum to search.
+    Returns:
+        the indexes of the two numbers.
+    """
+    # first create a hash map
+    map = {nums[i]: i for i in range(len(nums))}
+    # then search target using the hash map
+    for i in range(len(nums)):
+        num = target - nums[i]
+        if num in map and i != map[num]:
+            return [i, map[num]]
+    return []
+
+
 def twoSum(nums: list[int], target: int) -> list[int]:
     """
+    Two sum: hash map while search.
     Args:
-        nums    the input array of numbers
-        target  the target sum to search
+        nums    the input array of numbers.
+        target  the target sum to search.
     Returns:
-        the indexes of the two numbers
+        the indexes of the two numbers.
     """
-
-    pass
+    map = {}
+    for i in range(len(nums)):
+        num = target - nums[i]
+        # search target at the same time: return if found
+        if num in map and i != map[num]:
+            return [i, map[num]]
+        # keep creating the hash map if not found
+        map[nums[i]] = i
+    return []
 
 
 if __name__ == "__main__":
     import json
-    print('Task 0001 - Two Sum:')
+    print("Task 0001 - Two Sum:")
+
     nums = [2, 7, 11, 15]
     target = 9
-    print(f'  target={target}, nums={json.dumps(nums)}')
+    print(f"  target={target}, nums={json.dumps(nums)}")
     re = twoSum(nums, target)
-    print(f'  result={json.dumps(re)}')
+    print(f"  result={json.dumps(re)}")
+
+    nums = [3, 2, 4]
+    target = 6
+    print(f"  target={target}, nums={json.dumps(nums)}")
+    re = twoSum(nums, target)
+    print(f"  result={json.dumps(re)}")
