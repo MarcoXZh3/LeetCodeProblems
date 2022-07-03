1debdf1a4b11349e8b8e5a896d7750b94ee76040
MarcoXZh3 <marcoxzh3@gmail.com>
2022-07-03 13:09:13 -0600

    Task 0002 - Add Two Numbers: finished

--- group01/task0002-add-two-num/add-two-num.js
+++ group01/task0002-add-two-num/add-two-num.js
@@ -8,8 +8,44 @@ const {ListNode, SinglyLinkedList} = require(path.resolve("_utils/list"));
  * @param {ListNode}  l2      the second list
  * @return {ListNode}         the sum of the two lists
  */
-const addTwoNumbers = (l1, l2) => {
+ const addTwoNumbers = (l1, l2) => {
+    const re = new ListNode(0);
+    let cur = re;
+    while (true) {
+        const sum = cur.val + (l1 ? l1.val : 0) + (l2 ? l2.val : 0);
+        cur.val = sum % 10;
+        // move next only if carry or at least 1 next available
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
+        if (l1 || l2 || sum > 9) {
+            cur.next = new ListNode(sum < 10 ? 0 : 1); // set carry as the init value
+            cur = cur.next;
+        } else {
+            break ;
+        }
+    }
+    return re;
+};
 
+
+/**
+ * Add two nums by recursion
+ * @param {ListNode}  l1      the first list
+ * @param {ListNode}  l2      the second list
+ * @return {ListNode}         the sum of the two lists
+ */
+ const addTwoNumbers2 = (l1, l2) => {
+    const recursion = (l1, l2, re) => {
+        sum = re.val + (l1 ? l1.val : 0) + (l2 ? l2.val : 0);
+        re.val = sum % 10;
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
+        if (l1 || l2 || sum > 9) {
+            re.next = recursion(l1, l2, new ListNode(sum < 10 ? 0 : 1));
+        }
+        return re;
+    };
+    return recursion(l1, l2, new ListNode(0));
 };
 
 
@@ -38,6 +74,7 @@ const main = () => {
         console.log(`+ ${SinglyLinkedList.printList(l2)}`);
         re = addTwoNumbers(l1, l2);
         console.log(`= ${SinglyLinkedList.printList(re)}`);
+        console.log("================================");
     });
 };
 
--- group01/task0002-add-two-num/add-two-num.py
+++ group01/task0002-add-two-num/add-two-num.py
@@ -9,8 +9,41 @@ def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
     Returns:
         the sum of the two number.
     """
+    re = ListNode(0)
+    cur = re
+    # repeat whenever there is at least 1 number
+    while True:
+        sum = cur.val + (l1.val if l1 else 0) + (l2.val if l2 else 0)
+        cur.val = sum % 10
+        # move next only if carry or at least 1 next available
+        l1 = l1.next if l1 else None
+        l2 = l2.next if l2 else None
+        if l1 or l2 or sum > 9:
+            cur.next = ListNode(0 if sum < 10 else 1)
+            cur = cur.next
+        else:
+            break
+    return re
 
-    pass
+
+def addTwoNumbers2(l1: ListNode, l2: ListNode) -> ListNode:
+    """Add two numbers by recursion
+    Args:
+        l1: the 1st number.
+        l2: the 2nd number.
+    Returns:
+        the sum of the two number.
+    """
+    def recursion(l1: ListNode, l2: ListNode, re: ListNode) -> ListNode:
+        sum = re.val + (l1.val if l1 else 0) + (l2.val if l2 else 0)
+        re.val = sum % 10
+        l1 = l1.next if l1 else None
+        l2 = l2.next if l2 else None
+        if l1 or l2 or sum > 9:
+            re.next = recursion(l1, l2, ListNode(0 if sum < 10 else 1))
+        return re
+
+    return recursion(l1, l2, ListNode(0))
 
 
 if __name__ == "__main__":
@@ -36,3 +69,4 @@ if __name__ == "__main__":
         print(f"+ {SinglyLinkedList.printList(l2)}")
         re = addTwoNumbers(l1, l2)
         print(f"= {SinglyLinkedList.printList(re)}")
+        print("================================")
